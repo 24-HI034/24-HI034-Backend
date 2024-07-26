4c848c6ab44797948de43cc33107305b08cb16cf
kyounghoonJang <matkimchi_@naver.com>
2024-07-27 02:04:56 +0900

    test

--- chatbot/app/main.py
+++ chatbot/app/main.py
@@ -2,22 +2,22 @@ from fastapi import FastAPI
 app = FastAPI()
 
 # 기본 경로
-@app.get("/second")
+@app.get("/chatbot")
 def read_root():
     return {"Hello": "World"}
 
 # 경로 매개변수
-@app.get("/second/ttt")
+@app.get("/chatbot/ttt")
 def read_item():
     return "test1"
 
 # POST 요청 처리
-@app.get("/second/tt")
+@app.get("/chatbot/tt")
 def create_item():
     return "test2"
 
 # POST 요청 처리
-@app.get("/second/ttttt")
+@app.get("/chatbot/ttttt")
 def create_item():
     return "test2"
 
